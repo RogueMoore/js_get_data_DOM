f3c3409eacaa5ce6d1dc28ae6964a6ff79cde302
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-10-09 15:05:45 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const populationData = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const total = [...populationData].reduce((total, country) => {
+  const clearingString = country.textContent.replaceAll(',', '');
+
+  const toNumber = parseInt(clearingString);
+
+  return total + toNumber;
+}, 0);
+
+const totalResult = (totalPopulation.textContent =
+  total.toLocaleString('en-us'));
+
+const averageResult = Math.round(total / populationData.length).toLocaleString(
+  'en-us',
+);
+
+averagePopulation.textContent = averageResult;
